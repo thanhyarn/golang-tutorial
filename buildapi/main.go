34864297c5df083771240c99fa89a5c8f648e811
Thanh Yarn <93782548+thanhyarn@users.noreply.github.com>
2024-02-07 18:28:06 +0700

    Add files via upload

--- buildapi/main.go
+++ buildapi/main.go
@@ -3,92 +3,145 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"math/rand" // Add this line to import the rand package
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
 // Model for course - file
-type Course struct{
-	CourseId 	string	`json: "courseid"`
-	CourseName	string 	`json: "coursename"`
-	CoursePrice int		`json: "price"`
-	Author 		*Author	`json: "author"`
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
 }
 
-type Author struct{
+type Author struct {
 	Fullname string `json:"fullname"`
-	Website	 string `json:"website"`
+	Website  string `json:"website"`
 }
 
 var courses []Course
 
 //middleware , helper - file
-func (c *Course) IsEmpty() bool{
+func (c *Course) IsEmpty() bool {
 	// return c.CourseId == "" && c.CourseName == ""
 	return c.CourseName == ""
 }
 
-func main(){
+func main() {
+	fmt.Println("API - Welcome")
+	r := mux.NewRouter()
 
+	//seeding
+	courses = append(courses, Course{CourseId: "2", CourseName: "ReactJS", CoursePrice: 299, Author: &Author{Fullname: "Thanh Yarn", Website: "yarndev.wordpress.com"}})
+	courses = append(courses, Course{CourseId: "4", CourseName: "MERN", CoursePrice: 199, Author: &Author{Fullname: "Thanh Yarn", Website: "yarn.go.dev"}})
+
+	//routing
+	r.HandleFunc("/", serveHome).Methods("GET")
+	r.HandleFunc("/courses", getAllCourses).Methods("GET")
+	r.HandleFunc("/course/{id}", getOneCourse).Methods("GET")
+	r.HandleFunc("/course", createOneCourse).Methods("POST")
+	r.HandleFunc("/course/{id}", updateOneCourse).Methods("PUT")
+	r.HandleFunc("/course/{id}", deleteOneCourse).Methods("DELETE")
+	//listen to a port
+	log.Fatal(http.ListenAndServe(":4000", r))
 }
 
-//controller - file 
+//controller - file
 
 // serve home route
-
-func serveHome(w http.ResponseWriter, r *http.Request){
+func serveHome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Welcome to API</h1>"))
 }
 
-func getAllCourses(w http.ResponseWriter, r *http.Request){
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get all courses")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses)
 }
 
-func getOneCourse(w http.ResponseWriter, r *http.Request){
-	fmt.Println("Get on course")
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course")
 	w.Header().Set("Content-Type", "application/json")
 
 	//grab id from request
-	params := mux.Vars()
+	params := mux.Vars(r)
 
 	//loop through courses, find matching id and return the response
-	for _, course := range courses{
-		if course.CourseId == params["id"]{
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
 			json.NewEncoder(w).Encode(course)
 			return
 		}
 	}
 	json.NewEncoder(w).Encode("No Course found with given id")
-	return 
 }
 
-func createOneCourse(w http.ResponseWriter, r *http.Request){
-	fmt.Println("Create on course")
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create one course")
 	w.Header().Set("Content-Type", "application/json")
 
 	// what if: body is empty
-	if r.Body == nil{
+	if r.Body == nil {
 		json.NewEncoder(w).Encode("Please send some data")
+		return
 	}
 
 	// what about - {}
 
 	var course Course
 	_ = json.NewDecoder(r.Body).Decode(&course)
-	if course.IsEmpty(){
+	if course.IsEmpty() {
 		json.NewEncoder(w).Encode("No data inside JSON")
 		return
 	}
 
-	// generate unique id , string 
+	// generate unique id , string
 	// append course into courses
 
 	rand.Seed(time.Now().UnixNano())
 	course.CourseId = strconv.Itoa(rand.Intn(100))
 	courses = append(courses, course)
-	return 
 }
 
+func updateOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	//first - grab id from req
+	params := mux.Vars(r)
+
+	// loop, id , remove , add with my ID
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			var newCourse Course
+			_ = json.NewDecoder(r.Body).Decode(&newCourse)
+			newCourse.CourseId = params["id"]
+			courses = append(courses, newCourse)
+			json.NewEncoder(w).Encode(newCourse)
+			return
+		}
+	}
+	//TODO: send a response when id is not found
+}
+
+func deleteOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Delete one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	// loop , id , remove (index, index + 1)
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			break
+		}
+	}
+}
